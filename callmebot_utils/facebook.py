51354c5e43711372fc001b6ed4389b7d63b6b72f
SAKIB-SALIM <144510317+SAKIB-SALIM@users.noreply.github.com>
2025-03-11 20:22:58 +0600

    changed self.API_KEY to self._API_KEY in facebook.py

--- callmebot_utils/facebook.py
+++ callmebot_utils/facebook.py
@@ -4,11 +4,11 @@ BASE_URL = 'https://api.callmebot.com/facebook/send.php'
 
 class Facebook:
     def __init__(self,API_KEY : str):
-        self.API_KEY = API_KEY
+        self._API_KEY = API_KEY
 
     def send_text(self,text : str):
         params={
-                'apikey' : self.API_KEY,
+                'apikey' : self._API_KEY,
                 'text' : text,
                 }
         response = requests.get(BASE_URL,params=params)
@@ -16,7 +16,7 @@ class Facebook:
 
     def send_image(self,image_url : str):
         params={
-                'apikey' : self.API_KEY,
+                'apikey' : self._API_KEY,
                 'image' : image_url,
                 }
         response = requests.get(BASE_URL,params=params)
